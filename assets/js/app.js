49c9f1a965bcd2e28d01f85743d6e625fd91b4bc
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-21 12:43:36 +0800

    add assets

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,10 @@
-const loader = document.querySelector(".loader");
+const loader = document.querySelector(".loader.center");
 
 setTimeout(() => {
   loader.style.top = "-200vh";
+  loader.style.opacity = "0";
 }, 3000);
+
+setTimeout(() => {
+  loader.style.display = "none";
+}, 4000);
